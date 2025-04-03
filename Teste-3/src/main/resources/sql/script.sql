fa24c71482a96e2cc652886962fa6c512f91abe7
Felipe Venas <dev.felipevenas@gmail.com>
2025-04-03 00:57:51 -0300

    "Criação das queries em SQL para criação das duas tabelas principais."

--- Teste-3/src/main/resources/sql/script.sql
+++ Teste-3/src/main/resources/sql/script.sql
@@ -1,18 +1,36 @@
-CREATE TABLE operadoras (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
-    cnpj VARCHAR(14),
+CREATE DATABASE automation_db;
+
+USE automation_db;
+
+CREATE TABLE IF NOT EXISTS operadoras_ativas
+(
+    registro_ans        VARCHAR(20) PRIMARY KEY,
+    cnpj                VARCHAR(18),
+    razao_social        VARCHAR(255),
+    nome_fantasia       VARCHAR(255),
+    modalidade          VARCHAR(100),
+    logradouro          VARCHAR(255),
+    numero              VARCHAR(10),
+    complemento         VARCHAR(100),
+    bairro              VARCHAR(100),
+    cidade              VARCHAR(100),
+    uf                  VARCHAR(2),
+    cep                 VARCHAR(20),
+    ddd                 VARCHAR(5),
+    telefone            VARCHAR(20),
+    endereco_eletronico VARCHAR(255),
+    representante       VARCHAR(255),
+    cargo_representante VARCHAR(255),
+    data_registro       DATE
 );
 
-CREATE TABLE demontracao_contabeis (
-    id SERIAL PRIMARY KEY,
-    operadora_id INT,
-    ano INT,
-    mes INT,
-    evento VARCHAR(255),
-    valor DECIMAL(15, 2),
-    FOREIGN KEY (operadora_id) REFERENCES operadoras(id)
+CREATE TABLE IF NOT EXISTS demonstrativos_contabeis
+(
+    registro_ans VARCHAR(20),
+    ano          INT,
+    trimestre    INT,
+    categoria    VARCHAR(255),
+    valor        DECIMAL(18, 2),
+    FOREIGN KEY (registro_ans) REFERENCES operadoras_ativas (registro_ans)
 );
 
-COPY operadoras(nome, cnpj, status)
-FROM './data/Relatorio_cadop.csv'
